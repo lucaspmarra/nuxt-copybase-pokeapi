7b5354548e892fd94db826fd763eb96a02d44338
Lucas Marra <marralucas1@gmail.com>
2022-10-20 21:42:30 -0300

    FEAT: update of _id page, working and getting data

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,40 @@
 <template>
   <div class="container">
     <h1>PokeAPI</h1>
-    <section v-if="loading">
-      <div class="spinner-border" role="status">
-        <span class="visually-hidden">Loading...</span>
-      </div>
-    </section>
-    <section v-if="error">
+
+    <article v-if="error">
       <p>
         Não foi possível realizar a requisição no momento, por favor, tente
         novamente mais tarde.
       </p>
       <p>Stacktrace: {{ error.message }}</p>
-    </section>
-    <section>
-      <b-form-input
-        v-model.lazy="pokemon"
-        class="mb-2 mr-sm-2 mb-sm-0 w-25"
-        type="search"
-        placeholder="Enter your name"
-      ></b-form-input>
-      <div class="mt-2">Value: {{ pokemon }}</div>
-      <b-button variant="primary" @click="onSubmit">Pesquisar</b-button>
-      <pre>{{ pokemons }}</pre>
-    </section>
+    </article>
+
+    <article>
+      <section class="mb-5">
+        <b-form-input
+          v-model="pokemonName"
+          class="mb-2 mr-sm-2 mb-sm-0 w-25"
+          type="search"
+          placeholder="Enter your name"
+        ></b-form-input>
+        <div class="mt-2">Value: {{ pokemonName }}</div>
+        <b-button variant="primary" @click="onSubmit"
+          >Search a pokemon</b-button
+        >
+      </section>
+
+      <section v-if="loading">
+        <div class="spinner-border" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+      </section>
+
+      <section class="mb-2">
+        <h4>Name:</h4>
+        <NuxtLink :to="`/pokemon/${pokemon.id}`" prefetch>{{ pokemon.name }}</NuxtLink>
+      </section>
+    </article>
   </div>
 </template>
 
@@ -32,8 +43,8 @@ export default {
   name: 'IndexPage',
   data() {
     return {
-      pokemon: '',
-      pokemons: [],
+      pokemonName: '',
+      pokemon: {},
       loading: false,
       error: false,
     }
@@ -42,10 +53,10 @@ export default {
     async onSubmit() {
       try {
         const response = await this.$axios.get(
-          `https://pokeapi.co/api/v2/pokemon/${this.pokemon}`
+          `https://pokeapi.co/api/v2/pokemon/${this.pokemonName}`
         )
-        this.pokemons = response.data
         this.loading = true
+        this.pokemon = response.data
         // eslint-disable-next-line no-console
         console.log(response.data)
       } catch (e) {
